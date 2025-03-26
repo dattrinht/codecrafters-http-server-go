144cdf2f8b1c2719204a8a5b8e40039d8fceb16e
Dat Trinh <dattrinh.net@gmail.com>
2025-03-27 00:02:57 +0700

    Return a file #ap6

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"strconv"
 
 	"github.com/codecrafters-io/http-server-starter-go/app/server"
@@ -39,6 +40,43 @@ func main() {
 				"Content-Type":   "text/plain",
 				"Content-Length": strconv.Itoa(len(userAgent)),
 			},
+		}
+	})
+
+	s.Handle("/files/{filename}", func(req *server.HttpRequest) *server.HttpResponse {
+		fileName := req.PathParams["filename"]
+
+		dir := ""
+		if len(os.Args) > 1 && os.Args[1] == "--directory" && len(os.Args) > 2 {
+			dir = os.Args[2]
+		}
+
+		if dir != "" {
+			fileName = dir + "/" + fileName
+		}
+
+		if _, err := os.Stat(fileName); err == nil {
+			if content, err := os.ReadFile(fileName); err == nil {
+				return &server.HttpResponse{
+					StatusCode:  200,
+					HttpVersion: req.HttpVersion,
+					Body:        string(content),
+					Headers: map[string]string{
+						"Content-Type":   "application/octet-stream",
+						"Content-Length": strconv.Itoa(len(content)),
+					},
+				}
+			} else {
+				return &server.HttpResponse{
+					StatusCode:  500,
+					HttpVersion: req.HttpVersion,
+				}
+			}
+		} else {
+			return &server.HttpResponse{
+				StatusCode:  404,
+				HttpVersion: req.HttpVersion,
+			}
 		}
 	})
 
